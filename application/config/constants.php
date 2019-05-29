4dda19849ff311571083a048b48e0f0e9f3721a7
Oscar Torres <40122963+JossueTorres@users.noreply.github.com>
2019-05-29 16:51:55 -0600

    Ejemplo de Form WebServices con Php

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,7 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+defined('COOKIECURL')      OR define('COOKIECURL', 'NID=67=pdjIQN5CUKVn0bRgAlqitBk7WHVivLsbLcr7QOWMn35Pq03N1WMy6kxYBPORtaQUPQrfMK4Yo0vVz8tH97ejX3q7P2lNuPjTOhwqaI2bXCgPGSDKkdFoiYIqXubR0cTJ48hIAaKQqiQi_lpoe6edhMglvOO9ynw; PREF=ID=52aa671013493765:U=0cfb5c96530d04e3:FF=0:LD=en:TM=1370266105:LM=1370341612:GM=1:S=Kcc6KUnZwWfy3cOl; OTZ=1800625_34_34__34_; S=talkgadget=38GaRzFbruDPtFjrghEtRw; SID=DQAAALoAAADHyIbtG3J_u2hwNi4N6UQWgXlwOAQL58VRB_0xQYbDiL2HA5zvefboor5YVmHc8Zt5lcA0LCd2Riv4WsW53ZbNCv8Qu_THhIvtRgdEZfgk26LrKmObye1wU62jESQoNdbapFAfEH_IGHSIA0ZKsZrHiWLGVpujKyUvHHGsZc_XZm4Z4tb2bbYWWYAv02mw2njnf4jiKP2QTxnlnKFK77UvWn4FFcahe-XTk8Jlqblu66AlkTGMZpU0BDlYMValdnU; HSID=A6VT_ZJ0ZSm8NTdFf; SSID=A9_PWUXbZLazoEskE; APISID=RSS_BK5QSEmzBxlS/ApSt2fMy1g36vrYvk; SAPISID=ZIMOP9lJ_E8SLdkL/A32W20hPpwgd5Kg1J'); // Cookie para ejecutar WS
+defined('USERAGENTWS')      OR define('USERAGENTWS', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13'); // Uagent para CURL
+defined('URLWS')      OR define('URLWS', 'http://104.248.185.225/practicaslab_utec/'); // url base para WS
--- application/controllers/c_admin/Edificio.php
+++ application/controllers/c_admin/Edificio.php
@@ -1,23 +1,72 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Edificio extends CI_Controller {
+class Edificio extends CI_Controller
+{
 
 	public function __construct()
 	{
-			parent::__construct();
-			//verificar la session de usuario
-			if(!$this->session->userdata("login")){
-					redirect(base_url());
-			}
+		parent::__construct();
+		//verificar la session de usuario
+		if (!$this->session->userdata("login")) {
+			redirect(base_url());
 		}
+	}
 
 	public function index()
 	{
+		//_________________________________________________________________
+		//Recojo y arreglo los parametros
+		$_param = array(
+			'cod' => 0,
+			'nom' => $this->input->post("txtNomFil"),
+			'acr' => $this->input->post("txtAcrFil"),
+			'est' => $this->input->post("ddlEstFil"),
+		);
+		$postData = '';
+		//Creamos arreglo nombre/valor separado por &
+		foreach ($_param as $k => $v) {
+			$postData .= $k . '=' . $v . '&';
+		}
+		rtrim($postData, '&');
+		//_________________________________________________________________
+
+
+		//_________________________________________________________________
+		//Recojo y arreglo los parametros
+		$url = URLWS . 'Edificio/Listado/';
+		//_________________________________________________________________
+
+
+		//_________________________________________________________________
+		//creamos nuevo recurso cURL y su Conf (Esto mejor que ni se toque siempre va)
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_USERAGENT, USERAGENTWS);
+		curl_setopt($ch, CURLOPT_COOKIE, COOKIECURL);
+		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
+		curl_setopt($ch, CURLOPT_POST, count($_param));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+		//_________________________________________________________________
+
+
+		//Obtenemos el resultado
+		//_________________________________________________________________
+		$data = json_decode(curl_exec($ch));		
+		//cerramos el Curl
+		curl_close($ch);
+		//_________________________________________________________________
+
+
 		$this->load->view('layouts/header');
 		$this->load->view('layouts/aside');
 		$this->load->view('layouts/nav');
-		$this->load->view('admin/edificios');
+		$this->load->view('admin/edificios', $data);
 		$this->load->view('layouts/footer');
+		
 	}
 }
